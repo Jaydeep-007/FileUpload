4c10115c8f751c3ba7910cb986bce7ba5fc93b1a
Jaydeep-007 <jaydeepvpatil07@gmail.com>
2022-10-01 18:37:38 +0530

    v1

---
+++ FileUpload/Program.cs
@@ -0,0 +1,28 @@
+using FileUpload.Data;
+using FileUpload.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DbContextClass>();
+builder.Services.AddScoped<IFileService, FileService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
